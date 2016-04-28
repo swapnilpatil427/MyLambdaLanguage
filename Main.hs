4a7c308230164ac9a950676d1f1f62764926dbaa
swapnil <swapnilpatil427@gmail.com>
2016-04-27 17:24:16 -0700

    main.hs changed

--- Main.hs
+++ Main.hs
@@ -3,15 +3,13 @@ import Eval
 
 test f = do
     x <- readFile f
-    --putStr x
---    x <- readFile f
- -- 	putStrLn x
-    putStrLn $ "***Testing " ++ x
+    putStrLn $ ""
+    putStrLn $ ""
+    putStrLn $ "*******Testing " ++ x
     let ast = ParsecParser.parseExpr x
     putStrLn $ "AST: " ++ (show ast)
-  --putStrLn $ "Source: " ++ (source ast)
+    putStrLn $ ""
     putStrLn $ "Eval: " ++ (show (eval (App (App (Abs "x" (Abs "y" (Binop Mul (Var "x") (Var "y")))) (Const 3)) (Const 5))))
-    --putStrLn $ "Eval: " ++ (show (eval (App (Abs "y" (Binop Mul (Var "y") (Var "y"))) (Const 3))))
 
 main = do
     test "examples/ex1.lam"
@@ -20,4 +18,3 @@ main = do
     test "examples/ex4.lam"
     test "examples/ex5.lam"
     test "examples/ex6.lam"
-    --putStr x
